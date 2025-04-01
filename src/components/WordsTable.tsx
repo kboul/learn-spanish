3e27da53914dde90d206cca47d849ab7f78af34f
kboul <kos.voulgaridis@gmail.com>
2025-04-01 15:30:48 +0300

    Change table width

--- src/components/WordsTable.tsx
+++ src/components/WordsTable.tsx
@@ -34,61 +34,63 @@ export function WordsTable({ words, error }: { words?: Word[]; error?: WordRespo
   };
 
   return (
-    <Table
-      data={words}
-      errorMsg={error}
-      headers={headers}
-      noItemsMsg="No words found"
-      renderRow={(word) => {
-        const { highlight, learned } = word;
-        const trBg = cn("bg-white", {
-          "bg-yellow-100": highlight,
-          "bg-[var(--learned)]": learned
-        });
-        const trColor = learned ? "text-white" : "text-black";
-        return (
-          <tr key={word.id} className="bg-white border-b border-gray-200">
-            <td className={`px-6 py-3 ${trBg}`}>
-              <p className={trColor}>{word.spanish}</p>
-            </td>
-            <td className={`px-6 py-3 ${trBg}`}>
-              <p className={trColor}>{word.english}</p>
-            </td>
-            <td className={`px-6 py-3 ${trBg}`}>
-              <p className={trColor}>{word.greek}</p>
-            </td>
-            <td className="px-6 py-3">
-              <div className="flex justify-center space-x-2 items-center">
-                <div
-                  className="cursor-pointer"
-                  onClick={() => handleWordMarkedAsLearned(word)}
-                  title={`Mark as ${learned ? "not " : ""}learned`}>
-                  {learned ? (
-                    <MdClear className={`${iconsClassName} text-red-700`} />
-                  ) : (
-                    <MdCheck className={`${iconsClassName} text-green-700`} />
-                  )}
-                </div>
-                {!word.learned && (
+    <div className="w-full max-w-4xl">
+      <Table
+        data={words}
+        errorMsg={error}
+        headers={headers}
+        noItemsMsg="No words found"
+        renderRow={(word) => {
+          const { highlight, learned } = word;
+          const trBg = cn("bg-white", {
+            "bg-yellow-100": highlight,
+            "bg-[var(--learned)]": learned
+          });
+          const trColor = learned ? "text-white" : "text-black";
+          return (
+            <tr key={word.id} className="bg-white border-b border-gray-200">
+              <td className={`px-6 py-3 ${trBg}`}>
+                <p className={trColor}>{word.spanish}</p>
+              </td>
+              <td className={`px-6 py-3 ${trBg}`}>
+                <p className={trColor}>{word.english}</p>
+              </td>
+              <td className={`px-6 py-3 ${trBg}`}>
+                <p className={trColor}>{word.greek}</p>
+              </td>
+              <td className="px-6 py-3">
+                <div className="flex justify-center space-x-2 items-center">
                   <div
                     className="cursor-pointer"
-                    onClick={() => handleWordHighlight(word)}
-                    title={`${highlight ? "Un" : "H"}ighlight word`}>
-                    {highlight ? (
-                      <MdFlashlightOff className={`${iconsClassName} text-blue-700`} />
+                    onClick={() => handleWordMarkedAsLearned(word)}
+                    title={`Mark as ${learned ? "not " : ""}learned`}>
+                    {learned ? (
+                      <MdClear className={`${iconsClassName} text-red-700`} />
                     ) : (
-                      <IoFlashlightSharp className={`${iconsClassName} text-blue-700`} />
+                      <MdCheck className={`${iconsClassName} text-green-700`} />
                     )}
                   </div>
-                )}
-                <div className="cursor-pointer" onClick={() => handleWordDelete(word.id)} title="Delete word">
-                  <IoTrashOutline className={`${iconsClassName} text-red-700`} />
+                  {!word.learned && (
+                    <div
+                      className="cursor-pointer"
+                      onClick={() => handleWordHighlight(word)}
+                      title={`${highlight ? "Un" : "H"}ighlight word`}>
+                      {highlight ? (
+                        <MdFlashlightOff className={`${iconsClassName} text-blue-700`} />
+                      ) : (
+                        <IoFlashlightSharp className={`${iconsClassName} text-blue-700`} />
+                      )}
+                    </div>
+                  )}
+                  <div className="cursor-pointer" onClick={() => handleWordDelete(word.id)} title="Delete word">
+                    <IoTrashOutline className={`${iconsClassName} text-red-700`} />
+                  </div>
                 </div>
-              </div>
-            </td>
-          </tr>
-        );
-      }}
-    />
+              </td>
+            </tr>
+          );
+        }}
+      />
+    </div>
   );
 }
